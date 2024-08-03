504086020f005e602b0ac2f5acaca4e6b4ae34a0
Alexy Cruz <ACruz@uvltd.com>
2024-08-03 11:59:50 -0600

    Chat component added, heroku url needs to be updated

---
+++ src/components/Chat.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import Message from './Message';
+import '../styles/Chat.css';
+
+const socket = io('https://your-heroku-app.herokuapp.com'); // Update with your Heroku app URL
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit('sendMessage', message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="messages">
+        {messages.map((msg, index) => (
+          <Message key={index} message={msg} />
+        ))}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message..."
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
